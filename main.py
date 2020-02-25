5d4e417859e1c8b779130edc1859198448a5f88f
afghanimah <afghanimah@gmail.com>
2020-02-25 14:03:45 -0600

    added if main check

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pyglet
 from game import Game
 
-pyglet.resource.path = ['resources/']
-pyglet.resource.reindex()
-window = Game()
-pyglet.clock.schedule_interval(window.update, 1/120.0)
-pyglet.app.run()
+if __name__ == "__main__":
+    pyglet.resource.path = ['resources/']
+    pyglet.resource.reindex()
+    window = Game()
+    pyglet.clock.schedule_interval(window.update, 1/120.0)
+    pyglet.app.run()
